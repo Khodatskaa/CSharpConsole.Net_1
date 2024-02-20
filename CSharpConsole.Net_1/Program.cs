3223b07fd8f6cd28c40d188f425cf17719e7a2e8
Khodatskaa <khodatskaalina@gmail.com>
2024-02-20 19:46:19 +0200

    Ex 4 was done

--- CSharpConsole.Net_1/Program.cs
+++ CSharpConsole.Net_1/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 
-namespace CSharpConsole.Net_1
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Enter the lower limit of the range: ");
+        if (int.TryParse(Console.ReadLine(), out int lowerLimit))   
         {
-            Console.WriteLine("");
+            Console.Write("Enter the upper limit of the range: ");
+            if (int.TryParse(Console.ReadLine(), out int upperLimit) && upperLimit >= lowerLimit)
+            {
+                DisplayFibonacciInRange(lowerLimit, upperLimit);
+            }
+            else
+            {
+                Console.WriteLine("Invalid upper limit. Please enter a valid integer greater or equal to the lower limit");
+            }
         }
+        else
+        {
+            Console.WriteLine("Invalid lower limit. Please enter a valid integer");
+        }
+    }
+
+    static void DisplayFibonacciInRange(int lowerLimit, int upperLimit)
+    {
+        int a = 0, b = 1, c = 0;
+
+        while (c <= upperLimit)
+        {
+            if (c >= lowerLimit)
+            {
+                Console.Write($"{c} ");      //($"{c} ") is used to display the value of c followed by a space
+            }
+
+            a = b;
+            b = c;
+            c = a + b;
+        }
+
+        Console.WriteLine(); 
     }
 }
