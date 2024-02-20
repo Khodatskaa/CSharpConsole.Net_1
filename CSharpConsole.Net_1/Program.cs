4c647d4d0472bd165b8db5ff7f19330980e1dd84
Khodatskaa <khodatskaalina@gmail.com>
2024-02-20 20:11:29 +0200

    Initial commit

--- CSharpConsole.Net_1/Program.cs
+++ CSharpConsole.Net_1/Program.cs
@@ -4,7 +4,7 @@ namespace CSharpConsole.Net_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("");
         }
