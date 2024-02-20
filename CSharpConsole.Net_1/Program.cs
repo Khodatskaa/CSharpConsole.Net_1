1b6ac6ba975bf828962b6df01220c9e27a77a78a
Khodatskaa <khodatskaalina@gmail.com>
2024-02-20 19:37:52 +0200

    Ex 3 was done

--- CSharpConsole.Net_1/Program.cs
+++ CSharpConsole.Net_1/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace CSharpConsole.Net_1
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Enter a six-digit number: ");
+        if (int.TryParse(Console.ReadLine(), out int userInput) && userInput >= 100000 && userInput <= 999999)
         {
-            Console.WriteLine("");
+            int reversedNumber = ReverseNumber(userInput);
+            Console.WriteLine($"Reversed number: {reversedNumber}");
         }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid six-digit number");
+        }
+    }
+
+    static int ReverseNumber(int number)
+    {
+        int reversedNumber = 0;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        return reversedNumber;
     }
 }
