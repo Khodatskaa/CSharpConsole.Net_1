1e535f25ccfc746e6357dd8f3aed38f3c8546ee9
Khodatskaa <khodatskaalina@gmail.com>
2024-02-20 19:34:07 +0200

    Ex 2 was done

--- CSharpConsole.Net_1/Program.cs
+++ CSharpConsole.Net_1/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 
-namespace CSharpConsole.Net_1
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        double[] numbers = new double[5];
+
+        for (int i = 0; i < 5; i++)
         {
-            Console.WriteLine("");
+            Console.Write($"Enter number {i + 1}: ");
+            if (double.TryParse(Console.ReadLine(), out double userInput))
+            {
+                numbers[i] = userInput;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number");
+                i--;
+            }
         }
+
+        double sum = 0;
+        double max = numbers[0];
+        double min = numbers[0];
+        double product = 1;
+
+        foreach (double num in numbers)
+        {
+            sum += num;
+            if (num > max) max = num;
+            if (num < min) min = num;
+            product *= num;
+        }
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Maximum: {max}");
+        Console.WriteLine($"Minimum: {min}");
+        Console.WriteLine($"Product: {product}");
     }
 }
