1caffdcf64516308fc32580e42b91bd15b6200db
Khodatskaa <khodatskaalina@gmail.com>
2024-02-21 00:05:57 +0200

    Ex 2 was done

--- CSharpConsole.Net_1/Program.cs
+++ CSharpConsole.Net_1/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace CSharpConsole.Net_1
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main()
+        Console.Write("Enter the number: ");
+        if (double.TryParse(Console.ReadLine(), out double value))
         {
-            Console.WriteLine("");
+            Console.Write("Enter the percentage: ");
+            if (double.TryParse(Console.ReadLine(), out double percentage))
+            {
+                double result = CalculatePercentage(value, percentage);
+                Console.WriteLine($"{percentage}% of {value} is: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for percentage. Please enter a valid number");
+            }
         }
+        else
+        {
+            Console.WriteLine("Invalid input for value. Please enter a valid number");
+        }
+    }
+
+    static double CalculatePercentage(double value, double percentage)
+    {
+        return (percentage / 100) * value;
     }
 }
