9ad36883f61b1ba0d38856e30d1062939b7d918a
Khodatskaa <khodatskaalina@gmail.com>
2024-02-21 00:22:52 +0200

    Ex 5 was done

--- CSharpConsole.Net_1/Program.cs
+++ CSharpConsole.Net_1/Program.cs
@@ -6,7 +6,38 @@ namespace CSharpConsole.Net_1
     {
         static void Main()
         {
-            Console.WriteLine("");
+            Console.WriteLine("Enter the date(dd.mm.yyyy): ");
+            if (DateTime.TryParseExact(Console.ReadLine(), "dd.mm.yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime inputDate))
+            {
+                string season = GetSeason(inputDate.Month);
+                string dayOfWeek = inputDate.DayOfWeek.ToString();
+
+                Console.WriteLine($"{season} {dayOfWeek}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid date format. Please enter a valid date in the format DD.MM.YYYY");
+            }
+        }
+
+        static string GetSeason(int month)
+        {
+            if (month == 12 || month == 1 || month == 2)
+            {
+                return "Winter";
+            }
+            else if (month >= 3 && month <= 5)
+            {
+                return "Spring";
+            }
+            else if (month >= 6 && month <= 8)
+            {
+                return "Summer";
+            }
+            else
+            {
+                return "Autumn";
+            }
         }
     }
 }
