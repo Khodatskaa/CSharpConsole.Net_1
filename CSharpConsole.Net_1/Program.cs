3868f8a62fa8f33d8056fdf9d7cfa65a75939bb7
Khodatskaa <khodatskaalina@gmail.com>
2024-02-20 20:00:58 +0200

    Ex 5 was done

--- CSharpConsole.Net_1/Program.cs
+++ CSharpConsole.Net_1/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace CSharpConsole.Net_1
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Enter the value of A (A < B): ");
+        if (int.TryParse(Console.ReadLine(), out int a))   // out int a - declare a variable to store the parsed value
         {
-            Console.WriteLine("");
+            Console.Write("Enter the value of B: ");
+            if (int.TryParse(Console.ReadLine(), out int b) && b > a)     // out int b - declare a variable to store the parsed value
+            {
+                DisplayNumberTriangle(a, b);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for B. Enter a valid integer greater than A");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input for A. Enter a valid integer");
+        }
+    }
+
+    static void DisplayNumberTriangle(int a, int b)
+    {
+        for (int i = a; i <= b; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine(); 
         }
     }
 }
